0fb333641be8c281c9e204ed07c4248173ff988d
Andrii-Pershko <andriiinf@gmail.com>
2023-01-12 01:20:47 +0200

    Complete Hw #2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galeryMarkUp = galleryItems.map(({ description, original, preview }, index) => {
+    return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" title="Image № ${index}"/>
+        </a>
+    `
+}).join(" ");
+
+galleryEl.insertAdjacentHTML('afterbegin', galeryMarkUp);
+
+var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250});
+
